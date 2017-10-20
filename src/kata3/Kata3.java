ca8d574140630ed96f85b2317274db201798a323
Dani <>
2017-10-20 18:12:34 +0100

    Kata3 versión 3 Ahora se añaden los datos repetidos usando bucles.

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -6,37 +6,10 @@ public class Kata3 {
 
         Histogram<String> histogram = new Histogram<>();
 
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("ull.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
-        histogram.increment("ulpgc.es");
+        for (int i = 0; i < 6; i++) histogram.increment("gmail.com");
+        for (int i = 0; i < 2; i++) histogram.increment("hotmail.com");
+        for (int i = 0; i < 8; i++) histogram.increment("ull.es");
+        for (int i = 0; i < 15; i++) histogram.increment("ulpgc.es");
 
         new HistogramDisplay(histogram).execute();
     }
